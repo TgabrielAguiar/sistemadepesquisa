aab8604625deb118d02eabcf80cfe76feb42ee6b
TgabrielAguiar <tallysongabriel3@gmail.com>
2018-07-19 14:52:27 -0300

    .

---
+++ template/contatos.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Sistema de pesquisa - Contato</title>
+    
+    <!-- VIEWPORT -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+	
+    <!-- RESET -->
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+    
+    <!-- CSS ALL -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    
+    <!-- FONTES LETRA -->
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans|Raleway" rel="stylesheet">
+    
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img class="logo-desktop" src="assets/img/logo/banner-desktop.png" alt="sistema de pesquisa">
+            </div>
+        </div>
+    </header>
+
+    
+<main>  
+    
+<section id="banner">
+    <img class="banner-desktop" src="assets/img/banner/banner01.jpg" alt=""/>
+</section>
+    
+<div class="montepc">
+    <div class="bgimgmp">
+        <div class="bgmpc container">
+            <h2>Contato</h2>
+            <p>Dúvidas, reclamações ou sujestões?</p>
+        </div>
+    </div>
+</div>    
+
+<div class="componentes container">
+    <div class="divcomp">
+        <div class="bgcomp">
+         <div class="bgcompcor">
+    <form method="POST" action="enviado.php"> 
+        <label><h3>Nome</h3></label>
+        <input type="text" name="nome" placeholder="Nome Completo"><br><br>
+
+        <label><h3>E-mail</h3></label>
+        <input type="email" name="email" placeholder="E-mail"><br><br><br>
+
+        <label><h3>Mensagem</h3></label>
+        <textarea type="text" name="mensagem" rows="5" cols="50"></textarea> ><br><br>
+
+    <input type="submit" value="Enviar"><br>
+
+    </div>
+</div>
+</div>s
+    </form>
+</div>    
+
+</div>
+</main>      
+</body>
+</html>
--- template/index.php
+++ template/index.php
@@ -52,7 +52,7 @@
                         <img src="assets/img/componentes/processador.png" alt="Processadores">
                         <h3>Processador</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Processador 1</option>
                         <option>Processador 2</option>
                         <option>Processador 3</option>
@@ -68,7 +68,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Placa Mãe">
                         <h3>Placa Mãe</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Placa Mãe 1</option>
                         <option>Placa Mãe 2</option>
                         <option>Placa Mãe 3</option>
@@ -84,7 +84,7 @@
                         <img src="assets/img/componentes/fan.png" alt="Water Cooler">
                         <h3>Water Cooler</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Water Cooler 1</option>
                         <option>Water Cooler 2</option>
                         <option>Water Cooler 3</option>
@@ -100,7 +100,7 @@
                         <img src="assets/img/componentes/fan.png" alt="Fans">
                         <h3>Fan</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Fan 1</option>
                         <option>Fan 2</option>
                         <option>Fan 3</option>
@@ -116,7 +116,7 @@
                         <img src="assets/img/componentes/ram.png" alt="Memória Ram">
                         <h3>Memória Ram</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Memória Ram 1</option>
                         <option>Memória Ram 2</option>
                         <option>Memória Ram 3</option>
@@ -132,7 +132,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Placa de Vídeo">
                         <h3>Placa de Vídeo</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Placa de Vídeo 1</option>
                         <option>Placa de Vídeo 2</option>
                         <option>Placa de Vídeo 3</option>
@@ -148,7 +148,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Hard Disk">
                         <h3>Hard Disk</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Hard Disk 1</option>
                         <option>Hard Disk 2</option>
                         <option>Hard Disk 3</option>
@@ -164,7 +164,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="SSD">
                         <h3>SSD</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>SSD 1</option>
                         <option>SSD 2</option>
                         <option>SSD 3</option>
@@ -180,7 +180,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Gravador">
                         <h3>Gravador</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Gravador 1</option>
                         <option>Gravador 2</option>
                         <option>Gravador 3</option>
@@ -196,7 +196,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Fonte">
                         <h3>Fonte</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Fonte 1</option>
                         <option>Fonte 2</option>
                         <option>Fonte 3</option>
@@ -212,7 +212,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Gabinete">
                         <h3>Gabinete</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Gabinete 1</option>
                         <option>Gabinete 2</option>
                         <option>Gabinete 3</option>
@@ -228,7 +228,7 @@
                         <img src="assets/img/componentes/placa-mae.png" alt="Monitor">
                         <h3>Monitor</h3>
                     </div>   
-                    <select>
+                    <select id="borda-escolha">
                         <option>Monitor 1</option>
                         <option>Monitor 2</option>
                         <option>Monitor 3</option>
